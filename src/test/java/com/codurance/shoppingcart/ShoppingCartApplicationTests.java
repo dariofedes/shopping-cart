396eeb6d4ce4795100ea88ab6a821aa97e48244d
dariofedes <dariofedes@protonmail.com>
2021-09-28 16:10:43 +0200

    initial commit

---
+++ src/test/java/com/codurance/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codurance.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
